127d058779e243f7421f72f3e29c68c520b86893
foodhype <brianschmitz.main@gmail.com>
2014-11-10 21:30:50 -0600

    done essentially

--- PriorityScheduler.h
+++ PriorityScheduler.h
@@ -1,6 +1,7 @@
 #ifndef PRIORITY_SCHEDULER_H
 #define PRIORITY_SCHEDULER_H
 
+#include <iostream>
 #include <queue>
 #include <thread>
 #include <mutex>
@@ -11,15 +12,19 @@ using namespace std;
 template <typename T>
 class PriorityScheduler {
     private:
+        bool done;
         priority_queue<T, vector<T>, greater<T> > q;
         mutex mtx;
         condition_variable cond;
 
-    public: 
+    public:
         void pop(T& item) {
             unique_lock<mutex> pop_lock(mtx);
             while (q.empty()) {
                 cond.wait(pop_lock);
+                if (done) {
+                    return;
+                }
             }
             item = q.top();
             q.pop();
@@ -34,6 +39,23 @@ class PriorityScheduler {
 
         bool empty() {
             return q.empty();
+        }
+
+        int size() {
+            return q.size();
+        }
+
+        void complete() {
+            done = true;
+            cond.notify_all();
+        }
+
+        bool is_complete() {
+            return done;
+        }
+
+        PriorityScheduler() {
+            done = false;
         }
 };
 
--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include "LIFOScheduler.h"
 #include "FIFOScheduler.h"
 #include "PriorityScheduler.h"
+#include <atomic>
 #include <cassert>
 #include <fstream>
 #include <iostream>
 #include <limits>
+#include <mutex>
 #include <papi.h>
 #include <string.h>
 #include <sstream>
@@ -12,6 +14,9 @@
 #include <queue>
 #include <vector>
 using namespace std;
+
+
+#define THREAD_COUNT 4
 
 
 class csr_graph {
@@ -28,7 +33,7 @@ csr_graph::csr_graph(vector<int> vertices, vector<pair<int, int> > edges) :
 }
 
 
-void relax(int current, pair<int, int> neighbor, int * dist,
+void relax(int current, pair<int, int> neighbor, int dist[],
         PriorityScheduler<pair<int, int> > &q) {
     int alt = dist[current] + neighbor.second;
 
@@ -38,8 +43,7 @@ void relax(int current, pair<int, int> neighbor, int * dist,
     }
 }
 
-
-int * dijkstra(csr_graph& graph, int source) {
+int * dijkstra(csr_graph& graph, int source, int thread_count) {
     int * dist = new int[graph.vertices.size()];
     for (int i = 0; i < graph.vertices.size(); ++i) {
         dist[i] = numeric_limits<int>::max();
@@ -80,14 +84,123 @@ int * dijkstra(csr_graph& graph, int source) {
 }
 
 
+void relax_work_stealing(int current, pair<int, int> neighbor, int dist[],
+        PriorityScheduler<pair<int, int> > &q) {
+    int alt = dist[current] + neighbor.second;
+
+    if (alt < dist[neighbor.first]) {
+        dist[neighbor.first] = alt;
+        q.push(make_pair(dist[neighbor.first], neighbor.first));
+    }
+}
+
+
+mutex mtx;
+atomic<bool> done(false);
+
+
+void work_stealing(csr_graph& graph, PriorityScheduler<pair<int, int> > schedulers[],
+            int tid, int dist[], int thread_count) {
+    int next = 0;
+    int current;
+    pair<int, int> popped;
+    while (!done) {
+        unique_lock<mutex> unq_lock(mtx);
+        if (schedulers[tid].empty()) {
+            // work stealing
+            /*
+            int max_thread = tid;
+            int max_workload = 1;
+            for (int index = 0; index < thread_count; ++index) {
+                if (schedulers[index].size() > max_workload) {
+                    max_workload = schedulers[index].size();
+                    max_thread = index;
+                }
+            }
+            schedulers[max_thread].pop(popped);
+            if (schedulers[max_thread].is_complete()) {
+                break;
+            }
+
+            schedulers[tid].push(popped);
+            */
+        } else {
+            // normal dijkstras
+            schedulers[tid].pop(popped);
+            if (dist[popped.second] != popped.first) {
+                continue;
+            }
+            current = popped.second;            
+
+            int start = graph.vertices[current];
+            int end = graph.vertices.size() + 1;
+            if (current + 1 < end) {
+                end = graph.vertices[current + 1];
+            }
+
+            for (int offset = start; offset < end; ++offset) {
+                next = (next + 1) % thread_count;
+                relax_work_stealing(current, graph.edges[offset], dist, ref(schedulers[next]));
+            }
+        }
+
+        bool is_done = true;
+        for (int index = 0; index < thread_count; ++index) {
+            if (!schedulers[index].empty()) {
+                is_done = false;
+            }
+        }
+
+        if (is_done) {
+            done = true;
+        }
+
+        unq_lock.unlock();
+    }
+
+    done = true;
+    cout << "done. tid: " << tid << " done: "<< done << endl;
+
+    for (int index = 0; index < thread_count; ++index) {
+        schedulers[index].complete();
+    }
+
+}
+
+
+int * dijkstra_work_stealing(csr_graph& graph, int source, int thread_count) {
+    int * dist = new int[graph.vertices.size()];
+    for (int i = 0; i < graph.vertices.size(); ++i) {
+        dist[i] = numeric_limits<int>::max();
+    }
+
+    dist[source] = 0;
+
+    PriorityScheduler<pair<int, int> > * schedulers = new PriorityScheduler<pair<int, int> >[thread_count];
+    schedulers[0].push(make_pair(dist[source], source));
+
+    vector<thread> threads;
+    for (int i = 0; i < thread_count; ++i) {
+        threads.push_back(thread(work_stealing, ref(graph), schedulers, i, dist, thread_count));
+    }
+
+    for (auto& t: threads) {
+        t.join();
+    }
+  
+    return dist;
+}
+
+
 int * profile(csr_graph& graph, int source) {
     char cache[10000000];
     memset(cache, 0, sizeof cache);
     
     long_long start_usec = PAPI_get_real_usec();
     
-    int * dist = dijkstra(graph, source);
-    
+    int * dist = dijkstra_work_stealing(graph, source, THREAD_COUNT);
+    done = false;
+
     long_long end_usec = PAPI_get_real_usec();
     long_long elapsed = end_usec - start_usec;
     cout << "sssp: " << elapsed << " usec elapsed " << endl;
@@ -173,6 +286,10 @@ csr_graph build_graph(string gr_filename) {
 int main() {
     csr_graph graph = build_graph("test.gr");
     int * dist = profile(graph, 0);
+
+    for (int i = 0; i < 9; ++i) {
+        cout << "dist[" << i << "] = " << dist[i] << endl;
+    }
 
     assert(dist[0] == 0);
     assert(dist[1] == 4);
